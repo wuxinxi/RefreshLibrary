e8b023a465482a840633a319c62901eafa036da3
wuxinxi <wu_tangren@163.com>
2017-06-16 18:24:34 +0800

    修改部分

--- TRefresh/src/main/java/com/wxx/refreshlibrary/recyclerView/PullRefreshAndLoadMoreView.java
+++ TRefresh/src/main/java/com/wxx/refreshlibrary/recyclerView/PullRefreshAndLoadMoreView.java
@@ -1,6 +1,8 @@
 package com.wxx.refreshlibrary.recyclerView;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.ColorRes;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.AttributeSet;
@@ -20,6 +22,10 @@ public class PullRefreshAndLoadMoreView extends LinearLayout implements SwipeRef
     private SwipeRefreshLayout refreshLayout;
     private TRecyclerView recyclerView;
 
+    private int[] colorRes = new int[]{android.R.color.holo_blue_bright,
+            android.R.color.holo_red_light,
+            android.R.color.holo_green_light};
+
     public PullRefreshAndLoadMoreView(Context context) {
         this(context, null);
     }
@@ -30,14 +36,23 @@ public class PullRefreshAndLoadMoreView extends LinearLayout implements SwipeRef
 
     public PullRefreshAndLoadMoreView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
         View view = LayoutInflater.from(context).inflate(R.layout.layout_refresh_load_view, this, true);
         recyclerView = (TRecyclerView) view.findViewById(R.id.recyclerView);
         refreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.refresh);
         refreshLayout.setOnRefreshListener(this);
         recyclerView.setOnLoadMoreListener(this);
+        refreshLayout.setColorSchemeResources(colorRes);
     }
 
 
+    public void setColorSchemeResources(@ColorRes int... colorResIds) {
+        if (colorResIds.length > 3)
+            throw new IllegalArgumentException("The length must be within 3 ");
+        for (int i = 0; i < colorResIds.length; i++) {
+            colorRes[i] = colorResIds[i];
+        }
+    }
 
     @Override
     public void onRefresh() {
